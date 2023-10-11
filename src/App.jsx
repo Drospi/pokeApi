517dc004206d7d8ab71b27de0030d6739fdcea8d
Drospi <drospigliozi@gmail.com>
2023-10-11 10:17:07 -0400

    v1.0

---
+++ src/App.jsx
@@ -0,0 +1,36 @@
+
+import { useEffect, useState } from 'react'
+import './App.css'
+import { Card } from './components/Card'
+
+function App() {
+  const [data, setData] = useState([])
+  const [pokemons, setPokemons] = useState([])
+  useEffect(()=>{
+    const getData = async()=>{
+      const res = await fetch('https://pokeapi.co/api/v2/pokemon?limit=100')
+      const datos = await res.json()
+      setData(datos.results)
+    }
+    getData()
+  },[])
+  
+  useEffect(()=>{
+    
+  const pokemon = []
+    data.map( poke =>{
+       fetch(`https://pokeapi.co/api/v2/pokemon/${poke.name}`)
+      .then(res=>res.json())
+      .then(data=>pokemon.push(data))
+    })
+    setPokemons(pokemon)
+  },[data])
+  console.log(pokemons);
+  return (
+    <>
+    <Card name={data.name} data={pokemons}></Card>
+    </>
+  )
+}
+
+export default App
---
+++ src/components/Card.jsx
@@ -0,0 +1,43 @@
+import { useState } from "react";
+
+export const Card = (el) => {
+  const [active, setActive] = useState(true);
+  const [num, setNum] = useState(0);
+  return (
+    <div className="todo">
+      <div className={active ? "caracteristicas d-none" : "caracteristicas"}>
+        <h1 onClick={() => setActive(true)}>x</h1>
+        <img src={el.data[num]?.sprites.other.home.front_default} alt="" />
+        <h3>Nombre: {el.data[num]?.forms[0].name}</h3>
+        <div>
+          <p>Habilidades</p>
+          <ul>
+            {el.data[num]?.abilities.map((abi, key) => {
+              return <li key={key}>{abi.ability.name}</li>;
+            })}
+          </ul>
+        </div>
+        <p>Altura: {el.data[num]?.height}</p>
+        <p>Peso: {el.data[num]?.weight}</p>
+        <p>Tipo: {el.data[num]?.types.map((typ)=>typ.type.name+' ')}</p>
+      </div>
+      <div className="all">
+        {el.data.map((poke, key) => {
+          return (
+            <div
+              className="card"
+              onClick={() => {
+                setActive(false);
+                setNum(key);
+              }}
+              key={key}
+            >
+              <img src={poke?.sprites.other.home.front_default} alt="" />
+              <h1>{poke?.forms[0].name}</h1>
+            </div>
+          );
+        })}
+      </div>
+    </div>
+  );
+};
